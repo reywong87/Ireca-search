2fc4bddcb33b43d87cde9d5aa3e5b2b08d04cc6a
Rey <interactivaomega@gmail.com>
2025-05-15 18:29:40 +0200

    install a configure firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,18 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {ApplicationConfig, provideZoneChangeDetection} from '@angular/core';
+import {provideRouter} from '@angular/router';
 
-import { routes } from './app.routes';
+import {routes} from './app.routes';
+import {initializeApp, provideFirebaseApp} from '@angular/fire/app';
+import {getAuth, provideAuth} from '@angular/fire/auth';
+import {getFirestore, provideFirestore} from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+    providers: [provideZoneChangeDetection({eventCoalescing: true}), provideRouter(routes), provideFirebaseApp(() => initializeApp({
+        projectId: "ireca-search",
+        appId: "1:313971213856:web:70c4a836f7db07497c5e45",
+        storageBucket: "ireca-search.firebasestorage.app",
+        apiKey: "AIzaSyBrDETQi1DNKmCiFZYzcgoQxCvlrG3ORhI",
+        authDomain: "ireca-search.firebaseapp.com",
+        messagingSenderId: "313971213856"
+    })), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())]
 };
